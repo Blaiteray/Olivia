4feb093e3f1e9fa2fcb750f8dcc2d4245984d753
Blaiteray <soumitra.phi@gmail.com>
2022-11-11 13:15:55 +0600

    making extension selector

--- UI/Library.py
+++ UI/Library.py
@@ -11,17 +11,62 @@ from kivy.uix.scrollview import ScrollView
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from pathlib  import Path
+from math import ceil
 from kivy.uix.spinner import Spinner
 from kivy.uix.popup import Popup
 
-class LibraryLayout(ScrollView):
+class LibraryLayout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rows=2
+        self.cols=1
+        self.menu_panel = BoxLayout(orientation='horizontal', size_hint_y=None, height= 33)
+        self.prev_button = Button(text='<<', size_hint=(None,None), height=33,width=66)
+        self.menu_panel.add_widget(self.prev_button)
+        self.add_widget(self.menu_panel)
+        self.folder_container = FolderContainer()
+        self.add_widget(self.folder_container)
+
+
+class FolderContainer(ScrollView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.scroll_distance = 100
-        self.scrollable_panel = BoxLayout(orientation='vertical',size_hint_y=None, height=33*30)
-        for i in range(30):
-            self.scrollable_panel.add_widget(Button(text='Try Me '+str(i), size_hint_y=None, height=33))
-        self.add_widget(self.scrollable_panel)
+
+        self.extension_folder = ExtensionFolder()
+        self.add_widget(self.extension_folder)
+        
+
+
+class ExtensionFolder(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.orientation = 'vertical'
+        self.size_hint_y = None
+        self.download_path = Path('./downloads')
+        self.extension_list = os.listdir(self.download_path)
+        height_scale = 150
+        folder_in_row = 5
+        folder_rows = ceil(len(self.extension_list)/folder_in_row)
+        self.height = folder_rows*height_scale
+
+        self.folder_container = GridLayout(rows=folder_rows, cols=folder_in_row, padding=[10,10,10,10], spacing=[10,10])
+        self.add_widget(self.folder_container)
+        for i in self.extension_list:
+            self.folder_container.add_widget(Button(text=i, size_hint_y=None, height=height_scale, size_hint_x=1/folder_in_row, on_press=self.extension_select_cb))
+        if len(self.extension_list) < folder_in_row:
+            self.folder_container.add_widget(Label(text='', size_hint_y=None, height=height_scale, size_hint_x=1/folder_in_row*(folder_in_row-len(self.extension_list)%folder_in_row)))
+
+    def extension_select_cb(self, i):
+        self.extension_path = self.download_path / i.text
+        print(self.extension_path)
+
+class MangaFolder:
+    pass
+
+
+class ChapterFolder:
+    pass
 
 
 
